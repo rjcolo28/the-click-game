7e980220a68afa6bfe56836a5e0acd04e230b683
Ryan Colosanti <silentcornball28@Ryans-MacBook-Pro.local>
2019-04-06 14:11:28 -0400

    Updates

---
+++ precache-manifest.fe46ef7db8fff54877c1e15eeb720c63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f423a739bb7af8e5d6fb",
+    "url": "/the-clicky-game/static/js/runtime~main.8c072800.js"
+  },
+  {
+    "revision": "97ad8a2b95c5ea77e2d9",
+    "url": "/the-clicky-game/static/js/main.046fd217.chunk.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/the-clicky-game/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "97ad8a2b95c5ea77e2d9",
+    "url": "/the-clicky-game/static/css/main.31171c56.chunk.css"
+  },
+  {
+    "revision": "6954e7c26f1b755154e3329647c71ad4",
+    "url": "/the-clicky-game/index.html"
+  }
+];
